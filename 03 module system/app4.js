8d8a2fca9ccf5550e7170f6c08fb3976ae15811e
atul074 <atultandan074@gmail.com>
2024-08-26 17:25:31 +0530

    clear

--- 03 module system/app4.js
+++ 03 module system/app4.js
@@ -1,5 +1,2 @@
 //npm install nodemon@1.18.5 -g
 
-//hello my name is atul and I have very small PP, its not my  fault that I am built like that.
-//I want to have a gf too but my PP size is too small to pull any living being that is a female...
-//i am pushing this commit to github to confess my little dark secret to all my github followers.
--- 04 file system & command line args/app.js
+++ 04 file system & command line args/app.js
@@ -1,2 +1,1 @@
 console.log(process.argv);
-atul.nunu();
