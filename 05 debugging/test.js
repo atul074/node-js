abe23c0691bcd08a490d1895d900411e3e9b848c
atul074 <atultandan074@gmail.com>
2024-09-04 20:57:16 +0530

    complete 05 section

---
+++ 05 debugging/test.js
@@ -0,0 +1,15 @@
+ import chalk from "chalk";
+//const chalk= require('chalk')
+const msg=chalk.green.inverse('hii..')
+console.log(msg);
+debugger
+console.log(chalk.redBright("testing"));
+
+//debugger                                                        put this on the code 
+//node inspect app.js add --title="course" --body="node.js"           run on terminal
+//node --inspect-brk app.js add --title="course" --body="node.js"
+
+//chrome://inspect                                                    open on chrome
+//chrome://inspect/#devices
+//esc key t open console on chrome
+// restart    run on terminal to restart the devtool of chrome 
