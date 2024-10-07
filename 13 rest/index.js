ad9df3c73316cc5b5dea350e3bcd02466ec2cd8a
atul074 <atultandan074@gmail.com>
2024-10-08 01:57:20 +0530

    upto lec -17 http

--- 13 rest/index.js
+++ 13 rest/index.js
@@ -7,7 +7,25 @@ const port=2024;
 //middleware -plugin
 app.use(express.urlencoded({extended:false}));
 
+app.use((req,res,next)=>{
+  //console.log("hello from middleware");
+  req.name="atul";
+  //return res.json({msg:"hello end"}) ye control yahi end karega aage nahi jane dega
+  fs.appendFile("log.txt",`\n${Date.now()}:${req.method}:${req.path}`
+  ,(err,data)=>{
+    next();
+  })
+
+})
+app.use((req,res,next)=>{
+ // console.log("hello from middleware 2 ",req.name);
+  next();
+})
+
 app.get("/api/users",(req,res)=>{
+  res.setHeader('X-myName','atul')  //custom header  X-denote custom header
+  console.log(req.headers);
+  
     return res.json(users);
 })
 app.get("/users",(req,res)=>{
@@ -22,6 +40,7 @@ app.route("/api/users/:id")
   .get((req,res)=>{
     const id=Number(req.params.id);
     const user=users.find((user)=>user.id===id);
+    if(!user) return res.status(404).json({error:"user not found"})
     return res.json(user);
   })
   .put((req,res)=>{ return res.json({status : "pending"})})
@@ -65,9 +84,11 @@ app.route("/api/users/:id")
 
 app.post("/api/users",(req,res)=>{
     const body=req.body;
+    if(!body || !body.first_name ||!body.email || !body.gender)
+      return res.status(400).json({msg:"all fields are required"});
     users.push({...body,id:users.length+1});
     fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-      return res.json({status : "success",id:users.length})
+      return res.status(201).json({status : "success",id:users.length})
     });
 })
 // app.patch("/api/users/:id",(req,res)=>{
